f26e780147fb7ac72d24cc8b03e75f73d9a128bc
gerchart <gerchart@Gercharts-MacBook-Pro.local>
2025-05-12 19:55:56 +0800

    update agent logs

--- agent/src/main.py
+++ agent/src/main.py
@@ -13,28 +13,36 @@ from WebSocketUploader import WebSocketUploader
 class MainApp:
     def __init__(self, config_file, logger:Logger):
         self.logger = logger
+        self.logger.info("Read config file")
         with open(config_file, 'r') as f:
             self.config = json.load(f)
         self.message_queue = asyncio.Queue(maxsize=1) # 定长队列
+        self.logger.info("Init SystemInfoCollector")
         self.collector = SystemInfoCollector(self.logger)
+        self.logger.info("Init WebSocketUploader")
         self.ws_client = WebSocketUploader(self.config['server_url'], self.config['secret'], self.logger)
 
     async def collect_info(self):
         while True:
+            self.logger.info("Collent sys info")
             self.collector.update_full_system_info()
             system_info = self.collector.get_full_system_info()
+            self.logger.info("Push sys info")
             await self.message_queue.put(system_info)
             await asyncio.sleep(self.config['collect_interval'])
 
     async def upload_info(self):
         """从队列中取出数据并发送"""
         while True:
+            self.logger.info("Pop sys info")
             data = await self.message_queue.get()
+            self.logger.info("Sent sys info")
             await self.ws_client.send_data(data)
             await asyncio.sleep(self.config['upload_interval'])
 
     async def run(self):
         """运行所有任务"""
+        self.logger.info("Agent Start")
         await asyncio.gather(
             self.collect_info(),
             self.upload_info()
@@ -43,7 +51,7 @@ class MainApp:
 LOG_CONFIG = {
     "name": "my_app",
     "log_file": "app.log",
-    "log_level": logging.DEBUG,
+    "log_level": logging.INFO,
     "console": True,
     "file_rotation": {
         "maxBytes": 1024 * 1024 * 10,
